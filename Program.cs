404fd9f6885b5d862d5c94b306e4ce8c1888581c
Paulojesus85 <paulo.historia.usp@gmail.com>
2022-05-21 18:54:04 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioTabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Tabuada");
+
+            int numero, cont = 0;
+
+            Console.WriteLine("Digite um número para descobrir a sua tabuada:");
+            numero = int.Parse(Console.ReadLine());
+
+            while(cont<= 10)
+            {
+                Console.WriteLine($"{numero} X {cont} = {numero * cont}");
+                cont++;
+            }*/
+
+            /*Console.WriteLine("Tabuada do 1 ao 10");
+
+            int a = 0, b = 0;
+
+            while(a <= 10)
+            {
+                Console.WriteLine($"Tabuada do {a}");
+                while (b <= 10)
+                {
+                    Console.WriteLine($"{a} X {b} = {a * b}");
+                    b++;
+                }
+                b = 0;
+                a++;
+                Console.ReadKey();
+            }*/
+
+            Console.ReadKey();
+
+        }
+    }
+}
